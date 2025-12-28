5b1a9d8bf20c97110c054da14feaa2d5dd6881fa
sathu-codes <sathu07x@gmail.com>
2025-12-28 16:19:31 +0530

    code

--- Accound.h
+++ Accound.h
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Account{
+    private:
+        string firstname;
+        string lastname;
+        string phone_number;
+        string NIC_number;
+        string ethnicity;
+        string password;
+        int account_id;
+
+    public:     
+        Account(string name, string phone_number, string NIC_number, string ethnicity, string password){
+            this->firstname = firstname;
+            this->lastname = lastname;
+            this->phone_number = phone_number;
+            this->NIC_number = NIC_number;
+            this->ethnicity =   ethnicity;
+            this->password = password;
+        }
+            void name_assign();
+            void nic_check(int NIC_number);
+            void phone_number_check(int phone_number);
+            void password_check(string password);
+            void update_details(int account_id);
+            void delete_account(int account_id);
+            int account_id_assign();
+            void account_details();
+};
